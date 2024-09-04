16ddb2316b5f2ecf324a689e169f6a57a02691e1
Alibek Zhubekov <alibekzhubekov@Alibeks-MacBook-Pro.local>
2024-09-04 18:03:22 +0500

    Fix issue related to an update in the library

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ import random
 import time
 from urllib.parse import urlparse
 
-
 # Google API Setup
 scopes = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
 service_account_info = os.getenv('GOOGLE_SERVICE_ACCOUNT') or json.load(open('../credentials.json'))
@@ -35,12 +34,19 @@ records = sheet.get_all_records()  # Assumes first row is header
 
 # Selenium Setup
 chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")  # Run in headless mode for CI environments like GitHub Actions
+chrome_options.add_argument("--disable-gpu")  # Disable GPU acceleration
+chrome_options.add_argument("--no-sandbox")  # Avoid sandbox issues in some environments
+chrome_options.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems in certain environments
 
-# Add any Chrome options you need here
-service = Service(ChromeDriverManager().install())
+try:
+    # Use ChromeDriverManager to automatically manage ChromeDriver versions
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+except WebDriverException as e:
+    print(f"Failed to initialize WebDriver: {str(e)}")
+    raise
 
-driver = webdriver.Chrome(service=service, options=chrome_options)
 driver.maximize_window()
 
 for record in records:
@@ -59,7 +65,6 @@ for record in records:
         continue  # Skip the rest of the code in this loop iteration and move to the next record
     
     # If connection was successful, proceed with screenshot
-    # Before trying to set the window size
     if successful_connection:
         try:
             current_date = datetime.now().strftime('%Y-%m-%d')
@@ -85,7 +90,6 @@ for record in records:
         except (TimeoutException, WebDriverException, InvalidArgumentException) as e:
             print(f"Error while processing {url}: {str(e)}")
             continue  # Skip file upload for this record if there were any errors
-
 
     # Try to upload to Google Drive
     try:
